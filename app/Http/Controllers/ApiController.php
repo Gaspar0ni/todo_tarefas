b71d7c19c6f16a6d35d4b39336e84e089908ab4e
Guilherme <guilherme.vgp@outlook.com>
2022-04-13 16:02:02 -0300

    Adicionando todas tarefas do banco até o delete

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -42,15 +42,70 @@ class ApiController extends Controller
         return $array;
     }
 
-    public function readTodo(){
+    public function readTodo($id){
+        $array = ['error' => ''];
 
+        $todo = Todo::find($id);
+
+        if($todo){
+            $array['todo'] = $todo;
+        }else{
+            $array['error'] = 'A tarefa ' .$id. ' não existe.';
+        }
+
+
+        return $array;
     }
 
-    public function updateTodo(){
+    public function updateTodo($id, Request $request){
+        $array = ['error' => ''];
 
+        //validar os dados
+        $rules = [
+            'title' => 'min: 3',
+            'done' => 'boolean'       //aceita true, false, 0, 1, '0', '1'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            $array['error'] = $validator->messages();
+            return $array;
+        }
+
+        $title = $request->input('title');
+        $done = $request->input('done');
+
+        // var_dump($done)
+
+        //atualizar o item
+        $todo = Todo::find($id);
+        if($todo){
+
+            if($title){
+                $todo->title = $title;
+            }
+            if($done != NULL){
+                $todo->done = $done;
+            }
+
+            $todo->save();
+
+        }else{
+            $array['error'] = 'Tarefa ' .$id. ' não existe, logo, não pode ser atualizada.';
+        }
+
+
+        return $array;
     }
 
-    public function deleteTodo(){
+    public function deleteTodo($id){
+        $array = ['error' => ''];
 
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        // $todo->destroy($id);
+
+        return $array;
     }
 }
